cbdce1ed270327d1d870b3a5a99192eb03d080db
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-05 17:37:25 +0100

    quickfix

--- components/routes/app-routes.js
+++ components/routes/app-routes.js
@@ -3,14 +3,6 @@ const express = require('express');
 
 module.exports = () => {
 	const start = async ({ app }) => {
-		/**
-		 * This endpoint serves the app as a standalone version
-		 * @route GET /
-		 * @group Content - Everything about serving the app
-		 * @returns 200 - Sucessful response
-		 * @returns {ErrorServer.model} 500 - Server error
-		 */
-
 		app.use(express.static(path.resolve(`${__dirname}/../../react-app/build`)));
 
 		app.get('/*', (req, res) => res.sendFile(path.resolve(`${__dirname}/../../react-app/build/index.html`)));
