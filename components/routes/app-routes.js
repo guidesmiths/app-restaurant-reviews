c2d76e4da20fc68149d5af60681037b28ac3def8
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-11 18:25:54 +0100

    test env on heroku

--- components/routes/app-routes.js
+++ components/routes/app-routes.js
@@ -5,7 +5,7 @@ module.exports = () => {
 	const start = async ({ app }) => {
 		app.use(express.static(path.resolve(`${__dirname}/../../react-app/build`)));
 
-		app.get('/test', (req, res) => res.JSON(process.env.TEST));
+		app.get('/test', (req, res) => res.send({ hello: process.env.TEST }));
 
 		app.get('/*', (req, res) => res.sendFile(path.resolve(`${__dirname}/../../react-app/build/index.html`)));
 
