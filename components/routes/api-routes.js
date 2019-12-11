5396fb98eaad9688564e023e75d22d98e23dfc4b
Daniel Herrero <danielherrerohernando@gmail.com>
2019-12-11 18:20:24 +0100

    test

---
+++ components/routes/api-routes.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+
+// module.exports = () => {
+// 	const start = async ({ app }) => {
+
+// 		app.get('/api/v1/restaurants', (req, res) => res.sendFile(path.resolve(`${__dirname}/../../react-app/build/index.html`)));
+
+// 		return Promise.resolve();
+// 	};
+
+// 	return { start };
+// };
--- components/routes/app-routes.js
+++ components/routes/app-routes.js
@@ -5,6 +5,8 @@ module.exports = () => {
 	const start = async ({ app }) => {
 		app.use(express.static(path.resolve(`${__dirname}/../../react-app/build`)));
 
+		app.get('/test', (req, res) => res.JSON(process.env.TEST));
+
 		app.get('/*', (req, res) => res.sendFile(path.resolve(`${__dirname}/../../react-app/build/index.html`)));
 
 		return Promise.resolve();
