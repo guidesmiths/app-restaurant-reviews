15ef1c01092b8e895defa824e4570bc542dd6fad
Daniel Herrero <danielherrerohernando@gmail.com>
2020-01-30 16:53:12 +0100

    feature: playing with styled components

--- react-app/src/components/Loading.tsx
+++ react-app/src/components/Loading.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Loading = () => (
-	<div style={{ textAlign: 'center' }}>
-		<h1>LOADING</h1>
-	</div>
-);
+const Loading = styled.div`
+	margin: 100px auto;
+	width: 11em;
+	height: 11em;
+	border-radius: 50%;
+	border-left: 8px solid #00c6ae;
+	position: relative;
+	animation: rot 1.4s infinite linear;
+
+	@keyframes rot {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
+	}
+`;
 
 export default Loading;
