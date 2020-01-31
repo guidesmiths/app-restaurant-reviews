57f7d4a2dbe9dd8c0ae43465bde89826838ee6e0
Daniel Herrero <danielherrerohernando@gmail.com>
2020-01-31 17:25:23 +0100

    feature: navbar test

--- react-app/src/components/styled/index.tsx
+++ react-app/src/components/styled/index.tsx
@@ -62,20 +62,28 @@ export const StyledNavBar = styled.div`
 	position: fixed;
 	top: 0;
 	left: 0;
-	height: 100vh;
-	width: 80px;
 	box-sizing: border-box;
 	box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
 	z-index: 100;
-	background: white;
-	transition: width 0.7s, background 0.7s;
 	padding: 20px 0 0 20px;
-	&:hover {
-		width: 300px;
-		background: rgb(250, 250, 250);
+	background: white;
+	@media (min-width: 880px) {
+		height: 100vh;
+		width: 80px;
+		transition: width 0.7s, background 0.7s;
+		&:hover {
+			width: 300px;
+			background: rgb(250, 250, 250);
+		}
 	}
+
 	@media (max-width: 880px) {
 		height: 80px;
 		width: 100%;
+		transition: height 0.7s, background 0.7s;
+		&:hover {
+			height: 320px;
+			background: rgb(250, 250, 250);
+		}
 	}
 `;
