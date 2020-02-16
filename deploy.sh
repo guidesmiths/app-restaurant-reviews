54dce3d30a265bd660640c03ecddc60a99cf214b
Daniel Herrero <danielherrerohernando@gmail.com>
2020-02-16 22:10:48 +0100

    fix: script params

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-echo "$0" | docker login --username=_ --password-stdin registry.heroku.com;
-heroku container:push web -a $1;
-heroku container:release web -a $1;
+echo "$1" | docker login --username=_ --password-stdin registry.heroku.com;
+heroku container:push web -a $2;
+heroku container:release web -a $2;
