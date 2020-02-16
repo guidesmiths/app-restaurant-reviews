b4bdee89caefb231e6d02f5826cf9e9f94d33535
Daniel Herrero <danielherrerohernando@gmail.com>
2020-02-16 22:02:56 +0100

    cicd: updating deploy bash script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com;
-heroku container:push web -a $APP_NAME;
-heroku container:release web -a $APP_NAME;
+echo "$0" | docker login --username=_ --password-stdin registry.heroku.com;
+heroku container:push web -a $1;
+heroku container:release web -a $1;
