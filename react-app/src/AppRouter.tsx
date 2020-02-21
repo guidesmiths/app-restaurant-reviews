e394d5f53e14a10802520313fe425f6446f07134
Daniel Herrero <danielherrerohernando@gmail.com>
2020-02-21 10:25:55 +0100

    fix: improving router with conditional login context

--- react-app/src/AppRouter.tsx
+++ react-app/src/AppRouter.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { LoginContext } from './context/LoginContext';
 import history from './history';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 
-const AppRouter = () => (
-	<Router history={history}>
-		<Switch>
-			<Route path="/login" exact component={Login} />
-			<Route path="/home" exact component={Home} />
-			<Route render={() => <Redirect to="/home" />} />
-		</Switch>
-	</Router>
-);
+const AppRouter = () => {
+	const { loginState } = useContext(LoginContext);
+	return (
+		<Router history={history}>
+			<Switch>
+				<Route path="/login" exact component={Login} />
+				<Route path="/home" exact component={Home} />
+				<Route render={() => (loginState ? <Redirect to="/home" /> : <Redirect to="/login" />)} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default AppRouter;
