8e2843eb7fa47b9c3ad1ec38c7007d8e5e9af85b
Daniel Herrero <danielherrerohernando@gmail.com>
2020-03-21 20:19:59 +0100

    feat: new routes

--- components/routes/api-routes.js
+++ components/routes/api-routes.js
@@ -1,10 +1,35 @@
 const axios = require('axios');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
 	const start = async ({ app, config }) => {
+		app.use(bodyParser.json());
 		app.get('/api/v1/restaurants', async (req, res) => {
 			try {
 				const { data } = await axios.get(`${config.url}/api/v1/restaurants`, {
+					headers: { Authorization: req.headers.authorization },
+				});
+				return res.json(data);
+			} catch (error) {
+				return res.json(error);
+			}
+		});
+
+		app.get('/api/v1/review/:restaurantId', async (req, res) => {
+			try {
+				const id = req.params.restaurantId;
+				const { data } = await axios.get(`${config.url}/api/v1/review/${id}`, {
+					headers: { Authorization: req.headers.authorization },
+				});
+				return res.json(data);
+			} catch (error) {
+				return res.json(error);
+			}
+		});
+
+		app.post('/api/v1/review', async (req, res) => {
+			try {
+				const { data } = await axios.post(`${config.url}/api/v1/review`, req.body, {
 					headers: { Authorization: req.headers.authorization },
 				});
 				return res.json(data);
